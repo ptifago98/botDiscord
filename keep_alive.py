3fb4c08c79993c85edf5322a091a418eb8a58b0a
ptifago98 <nathan.98@hotmail.com>
2026-03-06 18:56:03 +0100

    Keep_alive changing

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import requests
+import time
 from threading import Thread
 import os
 
@@ -7,6 +9,14 @@ app = Flask('')
 @app.route('/')
 def home():
     return "Bot is alive!"
+def self_ping():
+    url = os.environ.get("RENDER_EXTERNAL_URL", "http://localhost:8080")
+    while True:
+        time.sleep(240)
+        try:
+            requests.get(url)
+        except:
+            pass
 
 def run():
     # Render or Replit will set PORT; default to 8080 if not set
@@ -16,4 +26,9 @@ def run():
 def keep_alive():
     # Run the Flask server in a separate thread
     t = Thread(target=run)
+    t.daemon = True
     t.start()
+
+    p = Thread(target=self_ping)
+    p.daemon = True
+    p.start()
